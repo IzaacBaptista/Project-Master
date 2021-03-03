e2ecfa835d310268e5b8a0bad76114bce89594b3
Izaac Baptista <izaacbaptista@gmail.com>
2021-03-02 21:08:05 -0300

    Create LightVehicle.cs

---
+++ Models/LightVehicle.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class LightVehicle : Vehicle {
+        public int Id { set; get; }
+        public string Restrictions { set; get; }
+
+        public static readonly List<LightVehicle> LightVehicles = new List<LightVehicle>();
+        public LightVehicle(
+            string Brand,
+            string Model,
+            int Year,
+            double Price,
+            string Color,
+            string Restrictions
+        ) : base(Brand, Model, Year, Price) {
+            this.Id = LightVehicles.Count;
+            this.Restrictions = Restrictions;
+
+            LightVehicles.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nRestrições: " + this.Restrictions;
+        }
+
+        public override bool Equals (object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (obj.GetType () != this.GetType ()) {
+                return false;
+            }
+            LightVehicle lightVehicle = (LightVehicle) obj;
+            return this.GetHashCode () == lightVehicle.GetHashCode ();
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = (int) 2166136261;
+                // Suitable nullity checks etc, of course :)
+                hash = (hash * 16777619) ^ this.Id.GetHashCode();
+                return hash;
+            }
+        }
+
+        public static List<LightVehicle> GetLightVehicles() {
+            return LightVehicles;
+        }
+    }
+}
