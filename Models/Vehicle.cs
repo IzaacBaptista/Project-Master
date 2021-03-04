9f441ab8db6f1a08ec412abca29256f12d6a9053
Izaac Baptista <izaacbaptista@gmail.com>
2021-03-04 20:29:04 -0300

    Add files via upload

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -5,18 +5,21 @@ namespace Model
         protected string Model { set; get; } // Modelo
         protected int Year { set; get; }     // Ano
         protected double Price { set; get; } // Valor para locação
+        //protected string Color { set; get; } // Cor
 
         protected Vehicle(
             string Brand,
             string Model,
             int Year,
             double Price
+            //string Color 
         ) 
         {
             this.Brand = Brand;
             this.Model = Model;
             this.Year = Year;
             this.Price = Price;
+           // this.Color = Color;
         }
 
         public override string ToString() {
@@ -25,6 +28,7 @@ namespace Model
                 "\nModelo: "           + this.Model +
                 "\nAno: "              + this.Year +
                 "\nPreço de Locação: " + this.Price;
+                //"\nCor: "              + this.Color;
         }
     }
 }
